14e0737909d783592aeabb4286a9346329c3d5e9
yonas emha <yyemha1@gmail.com>
2020-04-15 23:14:33 -0500

    555

--- app.js
+++ app.js
@@ -75,6 +75,10 @@ mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex :true })
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
+
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+  })
 }
 
 const port = process.env.PORT || 5000
